52fb55a3cbd394de60ac901d6a7f8ebf7fe84e17
Jeremy Perez <glyde06@hotmail.com>
2016-08-17 00:00:53 -0500

    wired up the routing for each typeform result to correlate with a visitor show page based off the token associated with each result and visitor.

--- app/helpers/visitors_helper.rb
+++ app/helpers/visitors_helper.rb
@@ -2,4 +2,13 @@ module VisitorsHelper
 	def only_completed_survey_results(results)
 		return results.select{|result| result["completed"]=="1"}
 	end
+
+	def get_visitor_id(visitor_token)
+		visitor = Visitor.find_by(typeform_token: visitor_token)
+		unless visitor == nil
+			return visitor.id
+		else 
+			return 1
+		end
+	end
 end
