209205ee178f608ee0a548747f89207c938a45d7
Jeremy Perez <glyde06@hotmail.com>
2016-08-19 16:38:58 -0500

    removed the 'null: false' constrainton the picture column of the visitors table, since I'm thinking there might be aninstance where a visitor completes a survey without taking a picture.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819211208) do
+ActiveRecord::Schema.define(version: 20160819213404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20160819211208) do
   end
 
   create_table "visitors", force: :cascade do |t|
-    t.binary   "picture",      null: false
+    t.string   "picture"
     t.integer  "lock_version"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
